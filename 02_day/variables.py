dcc23165a4ba11392a176102bef81e309e5d076a
Stanislav Dolgachov <s.dolgachov@gmail.com>
2026-09-05 20:54:21 +0300

    Add Day2 basic challenge

---
+++ 02_day/variables.py
@@ -0,0 +1,16 @@
+# Day 2: 30 Days of python programming
+
+first_name: int = "Stan"
+last_name: int = "Stan"
+
+full_name = f"{first_name} {last_name}"
+country = 'Ukraine'
+city = "Kyiv"
+age = 25
+year = 2026
+
+is_married = True
+is_true: bool = False
+is_light_on = True
+
+my_var, other = 1, 2
