53c291b1bd7a627678c7a8b00edf2a2899ad20c1
Stanislav Dolgachov <s.dolgachov@gmail.com>
2026-09-06 23:36:41 +0300

    add some stuff to variables

--- 02_day/variables.py
+++ 02_day/variables.py
@@ -14,3 +14,20 @@ is_true: bool = False
 is_light_on = True
 
 my_var, other = 1, 2
+
+# ======== 
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(age))
+print(type(is_true))
+
+print(len(first_name))
+print(len(first_name) == len(last_name))
+
+num_one = 5
+num_two = 4
+
+total = num_one + num_two
+diff = num_one - num_two
